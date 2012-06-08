f72b810e9dc6f1cd9a684df5bf0c8c1dcb0bce4a
Reid Gillette <reid.gillette@gmail.com>
2012-06-08 17:08:56 -0500

    Store moves by player instead

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,24 @@
 class Board
   def initialize
-    @moves = {}
+    @moves = { "X" => [], "O" => [] }
   end
   
   def make_move(player, move)
     move = move.to_i
     if valid_move?(move)
-      @moves[move] = player
+      @moves[player.token] << move
     else
       false
     end
   end
   
   def valid_move?(move)
-    !@moves.keys.include?(move) && ( move > 0 && move < 10 )
+    !occupied_spaces.include?(move) && ( move > 0 && move < 10 )
+  end
+  
+  def occupied_spaces
+    @moves.inject([]) do |occupied, (player, moves)|
+      occupied | moves
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,10 +2,12 @@ require './lib/board.rb'
 
 describe Board do
   let(:board) { Board.new }
-  let(:player) { double("Player") }
+  let(:player1) { double("Player", token: "X") }
+  let(:player2) { double("Player", token: "O") }
+  
   describe "#new" do
     it "has no moves" do
-      board.instance_variable_get(:@moves).should == {}
+      board.instance_variable_get(:@moves).should == { "X" => [], "O" => [] }
     end
   end
   
@@ -16,8 +18,8 @@ describe Board do
       end
       
       it "adds a move to the list of moves" do
-        board.make_move(player, "1")
-        board.instance_variable_get(:@moves).should == { 1 => player }
+        board.make_move(player1, "1")
+        board.instance_variable_get(:@moves).should == { "X" => [1], "O" => [] }
       end
     end
     
@@ -27,19 +29,19 @@ describe Board do
       end
       
       it "returns false" do
-        board.make_move(player, "1").should == false
+        board.make_move(player1, "1").should == false
       end
       
       it "doesn't record it" do
-        board.make_move(player, "1")
-        board.instance_variable_get(:@moves).should == { }
+        board.make_move(player1, "1")
+        board.instance_variable_get(:@moves).should == { "X" => [], "O" => [] }
       end
     end
   end
   
   describe ".valid_move?" do
     before :each do      
-      board.instance_variable_set(:@moves, { 1 => player, 5 => player})
+      board.instance_variable_set(:@moves, { "X" => [1], "O" => [5] } )
     end
     
     it "is true if the space is open" do
@@ -53,6 +55,17 @@ describe Board do
     it "is false if the space is not on the board" do
       board.valid_move?(-3).should == false
       board.valid_move?(20).should == false
+    end
+  end
+  
+  describe ".occupied_spaces" do
+    it "is empty with no moves" do
+      board.occupied_spaces.should == []
+    end
+    
+    it "contains all occupied spaces" do
+      board.instance_variable_set(:@moves, { "X" => [1], "O" => [5] } )
+      board.occupied_spaces.should == [1, 5]
     end
   end
 end
