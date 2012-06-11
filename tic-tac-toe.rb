291b038ae4b5965a7df3950dd0b4f988d422c9b8
Reid Gillette <reid.gillette@gmail.com>
2012-06-10 22:41:17 -0500

    Set up tic-tac-toe game

---
+++ tic-tac-toe.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+
+game = Game.new
+
+until game.winner do
+  game.draw_board
+  puts "What is your move?"
+  game.take_turn
+end
+
+game.draw_board
+puts "Who wins? #{game.winner}"
